64f46bf2de2eb43109617c72f9670cbfd89b7e96
cammac <camm@rocktechcomputer.com>
2021-02-23 10:14:16 -0800

    added before and after methods

--- src/test/java/junit5tests/FirstTestClass.java
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -1,9 +1,29 @@
 package junit5tests;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FirstTestClass {
+
+    @BeforeAll
+    void beforeAll() {
+        System.out.println("--This is the beforeAll method");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("----This is the beforeEach method");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println("--This is the afterAll method");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("----This is the afterEach method");
+    }
 
     @Test
     void firstMethod() {
