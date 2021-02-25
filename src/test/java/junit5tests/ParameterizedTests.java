f11b9ee780e7ec7d22801dcbd49cad902e41959a
cammac <camm@rocktechcomputer.com>
2021-02-25 15:06:32 -0800

    finished param csvSource tests

--- src/test/java/junit5tests/ParameterizedTests.java
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -1,6 +1,7 @@
 package junit5tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -19,6 +20,31 @@ public class ParameterizedTests {
     @ValueSource(strings = {"firstString", "secondString"})
     void stringValues(String theParam) {
         System.out.println("theParam = " + theParam);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,rogers", "captain, marvel", "bucky,barnes"})
+    void csvSource_StringString(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,32,true", "captain,21,false", "bucky,5,true"})
+    void csvSource_StringIntBoolean(String param1, int param2, boolean param3) {
+        System.out.println("param1: " + param1 + ", param2: " + param2 + ", param3: " + param3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"captain america, 'steve,rogers'",
+                        "winter soldier, 'bucky,barnes'"})
+    void csvSource_StringWithComma(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve?rogers", "bucky?barnes"}, delimiter = '?')
+    void csvSource_StringWithDiffDelim(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
     }
 
 }
